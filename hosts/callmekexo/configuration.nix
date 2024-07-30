794ae2206bb5f776f2095944006a1772c8c23092
Davi Reis <daviavr@gmail.com>
2024-07-30 00:36:05 -0300

    adding firefox configs

--- hosts/callmekexo/configuration.nix
+++ hosts/callmekexo/configuration.nix
@@ -68,7 +68,30 @@
     shell = pkgs.zsh;
   };
 
-  programs.firefox.enable = true;
+  programs.firefox = { 
+    enable = true;
+    policies = {
+        DisableTelemetry = true;
+        DisableFirefoxStudies = true;
+        EnableTrackingProtection = {
+          Value= true;
+          Locked = true;
+          Cryptomining = true;
+          Fingerprinting = true;
+        };
+        DisablePocket = true;
+        DisableFirefoxAccounts = true;
+        DisableAccounts = true;
+        DisableFirefoxScreenshots = true;
+        OverrideFirstRunPage = "";
+        OverridePostUpdatePage = "";
+        DontCheckDefaultBrowser = true;
+        DisplayBookmarksToolbar = "newtab"; # alternatives: "always" or "newtab"
+        DisplayMenuBar = "default-off"; # alternatives: "always", "never" or "default-on"
+        SearchBar = "unified"; # alternative: "separate"
+    };
+
+  };
   programs.zsh.enable = true;
 
   nixpkgs.config.allowUnfree = true;
