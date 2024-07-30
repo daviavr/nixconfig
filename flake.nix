cd86ab9f49eb8a6e9b13f4a116334ac1eca49ca4
Davi Reis <daviavr@gmail.com>
2024-07-30 00:20:28 -0300

    leaving thorium derivation i guess but no idea how to use it

--- flake.nix
+++ flake.nix
@@ -11,10 +11,9 @@
   };
 
   outputs = { self, nixpkgs, home-manager, flatpaks, ... }@inputs: {
-    packages = forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     nixosConfigurations.callmekexo = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      specialArgs = {inherit inputs outputs;};
+      specialArgs = {inherit inputs;};
       modules = [
         ./hosts/callmekexo/configuration.nix
 	home-manager.nixosModules.home-manager 
