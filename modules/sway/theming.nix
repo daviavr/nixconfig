a04f66cf93444a5c2a234a0d9754ecbc2d09383f
Davi Reis <daviavr@gmail.com>
2024-08-24 23:59:27 -0300

    do not talk to me ever again

--- modules/sway/theming.nix
+++ modules/sway/theming.nix
@@ -5,31 +5,20 @@ let
   cursorPackage = pkgs.bibata-cursors;
 in
 {
-  #environment.sessionVariables = lib.mkDefault config.home-manager.users.davi.home.sessionVariables;
   home-manager.users.davi = {
     dconf.settings = {
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
       };
     };
-    #home.file.".config/gtk-4.0/gtk-dark.css".text = "@import url(\"${pkgs.gnome.gnome-themes-extra}/share/themes/Adwaita-dark/gtk-4.0/gtk.css\");";
     gtk = {
       enable = true;
-      #theme = {
-      #  name = "Adwaita-dark";
-      #  package = pkgs.gnome.gnome-themes-extra;
-      #};
       gtk3 = {
         extraConfig.gtk-application-prefer-dark-theme = true;
       };
       gtk4 = {
         extraConfig.gtk-application-prefer-dark-theme = true;
       };
-    };
-    qt = {
-      enable = true;
-      platformTheme.name = "adwaita";
-      style.name = "adwaita-dark";
     };
 
     home.pointerCursor = {
