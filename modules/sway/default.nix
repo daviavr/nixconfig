b369715ed4e54f16eba64287bf30973f51acc21a
Davi Reis <daviavr@gmail.com>
2024-08-24 22:46:59 -0300

    adding auto unlocking of ssh keys using gnupg

--- modules/sway/default.nix
+++ modules/sway/default.nix
@@ -6,7 +6,7 @@ let
   cfg = config.modules.sway;
 in
 {
-  imports = [ ./theming.nix ./wofi ./waybar ];
+  imports = [ ./authentication.nix ./theming.nix ./wofi ./waybar ];
 
   options.modules.sway.enable = mkEnableOption "sway";
 
@@ -14,22 +14,6 @@ in
     services.xserver.enable = true;
     services.xserver.displayManager.gdm.enable = true;
     networking.networkmanager.enable = true;
-
-    security.polkit.enable = true;
-    services.gnome.gnome-keyring.enable = true;
-    systemd.user.services.polkit-gnome-authentication-agent-1 = {
-      description = "polkit-gnome-authentication-agent-1";
-      wantedBy = [ "graphical-session.target" ];
-      wants = [ "graphical-session.target" ];
-      after = [ "graphical-session.target" ];
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
-        Restart = "on-failure";
-        RestartSec = 1;
-        TimeoutStopSec = 10;
-      };
-    };
 
     xdg.portal = {
       enable = true;
