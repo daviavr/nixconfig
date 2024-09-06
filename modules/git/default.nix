e27cfd58e1f9747faa48e8921564ee32489cd58a
Davi Reis <daviavr@gmail.com>
2024-09-06 12:28:03 -0300

    adding my repo as a safe repository to 'bypass' a bug

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -7,10 +7,19 @@ in
 
   options = { modules.git.enable = mkEnableOption "git"; };
   config = mkIf cfg.enable {
+    programs.git = {
+      enable = true;
+      config = {
+        safe = {
+          "directory" = [ "/home/davi/nixconfig" ];
+        };
+      };
+    };
     home-manager.users.davi.programs.git = {
       enable = true;
       userName = "Davi Reis";
       userEmail = "daviavr@gmail.com";
+      extraConfig.init.defaultBranch = "main";
     };
   };
 }
