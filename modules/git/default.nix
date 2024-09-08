52f5b37e1a98619aa8314262639029871a8b6b0e
Davi Reis <daviavr@gmail.com>
2024-09-08 14:24:24 -0300

    adding nix config as safe dir to correct doas but/error

--- modules/git/default.nix
+++ modules/git/default.nix
@@ -7,19 +7,21 @@ in
 
   options = { modules.git.enable = mkEnableOption "git"; };
   config = mkIf cfg.enable {
-    programs.git = {
-      enable = true;
-      config = {
-        safe = {
-          "directory" = [ "/home/davi/nixconfig" ];
-        };
-      };
-    };
     home-manager.users.davi.programs.git = {
       enable = true;
       userName = "Davi Reis";
       userEmail = "daviavr@gmail.com";
-      extraConfig.init.defaultBranch = "main";
+
+      extraConfig = {
+        init.defaultBranch = "main";
+# adding nixconfig as safe dir
+# https://github.com/NixOS/nix/issues/6443
+# https://discourse.nixos.org/t/nixos-rebuild-switch-fails-under-flakes-and-doas-with-git-warning-about-dubious-ownership/46069/7
+        safe = {
+          directory = [ "$HOME/nixconfig" ];
+        };
+      };
+
     };
   };
 }
